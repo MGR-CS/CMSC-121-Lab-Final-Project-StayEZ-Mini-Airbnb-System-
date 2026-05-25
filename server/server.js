96cfad6c54f218be5bd3b277a3f7ac03a278c4b8
chrispy <chriseuandeleon@gmail.com>
2026-05-26 00:05:07 +0800

    Make server point to .env in root folder

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: require('path').resolve(__dirname, './.env') });
+require("dotenv").config({ path: require('path').resolve('./.env') });
 
 console.log("Current Working Directory:", process.cwd());
 console.log("Database URI from env:", process.env.MONGO_URI);
