0a3055ab0dce30a32d253f5b9707884319ac4ee9
Mark Gabrielle Rites <marites@up.edu.ph>
2026-05-26 09:12:55 +0800

    added some fixes to the favorites.html not writing to the actual db. added extra functionality in admin to change user roles and delete users.

--- server/models/User.js
+++ server/models/User.js
@@ -38,6 +38,9 @@ const userSchema = new mongoose.Schema(
 
 // Everytime a request is sent to mongoose to save data to user db, trigger this function
 userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
